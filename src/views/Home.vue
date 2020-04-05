a70991b01e84f3789ae1e6ff34fa95b541aab4e9
kerensoko86 <keren.sokoletsky@gmail.com>
2020-04-05 14:14:16 +0300

    filter+add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,30 @@
   <div class="home">
     <h1>Welcome to our page</h1>
     <addReview />
+    <reviewFilter class="input" @set-filter="setFilter" />
   </div>
 </template>
 
 <script>
-import addReview from "@/components/addReview.vue";
-// @ is an alias to /src
+import addReview from "@/components/add-review.vue";
+import reviewFilter from "@/components/review-filter.vue";
 
 export default {
   name: "Home",
   components: {
-    addReview
+    addReview,
+    reviewFilter
+  },
+  methods: {
+    setFilter(filterBy) {
+      this.$store.commit({
+        type: "filterReview",
+        filterBy
+      });
+      this.$store.dispatch({
+        type: "loadReviews"
+      });
+    }
   }
 };
 </script>
